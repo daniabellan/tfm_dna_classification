4ec141d69a588bc4f4e5077b058f6592f0471a08
Daniel Abellan <abellansanchezdaniel@gmail.com>
2025-02-16 11:42:19 +0100

    Experiments added

--- run_experiments.sh
+++ run_experiments.sh
@@ -9,7 +9,7 @@ CONFIG_DIR="configs/experiment_configs"
 
 # Lista de archivos específicos a ejecutar
 CONFIG_FILES=(
-    "2000s_k3_ecoli_salmonella.yaml"
+    "2000s_k3_ecoli_salmonella_signal.yaml"
 )
 
 # Activar Conda
